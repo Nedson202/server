a41502ba537fe45a8f8a40d75174af627f419f67
Samson <simeonsamson4@gmail.com>
2018-03-11 06:50:21 +0100

    Refactor

--- middlewares/business-filterer.js
+++ middlewares/business-filterer.js
@@ -1,33 +1,51 @@
 import businesses from '../model/business';
 
 export function sortQuery(req, res, next) {
-  const { location, category } = req.query;
+  const { location, category } = (req.query);
 
-  if (location) {
-    const filteredBusiness = businesses.filter(business =>
-      location.toLowerCase() === business.location);
-    if (filteredBusiness.length === 0) {
-      res.status(404).json({
-        message: 'Business not found',
-        error: true
-      });
-    }
-    res.status(200).json(filteredBusiness);
+  // if (location) {
+  //   const filteredBusiness = businesses.filter(business =>
+  //     location.toLowerCase() === business.location);
+  //   if (filteredBusiness.length === 0) {
+  //     res.status(404).json({
+  //       message: 'Business not found',
+  //       error: true
+  //     });
+  //   }
+  //   res.status(200).json(filteredBusiness);
+  // }
+  //
+  // if (category) {
+  //   const filteredBusiness = businesses.filter(business =>
+  //     category.toLowerCase() === business.category);
+  //   if (filteredBusiness.length === 0) {
+  //     res.status(404).json({
+  //       message: 'Business not found',
+  //       error: true
+  //     });
+  //   }
+  //   res.status(200).json(filteredBusiness);
+  // }
+  //
+  // next();
+
+  if (location || category) {
+    businesses.forEach((business) => {
+      if (location === business.location) {
+        res.status(200).json(business)
+      }
+      if (category === business.category) {
+        res.status(200).json(business)
+      }
+    })
+    res.status(404).json({
+      message: 'Business not found',
+      error: true
+    });
   }
 
-  if (category) {
-    const filteredBusiness = businesses.filter(business =>
-      category.toLowerCase() === business.category);
-    if (filteredBusiness.length === 0) {
-      res.status(404).json({
-        message: 'Business not found',
-        error: true
-      });
-    }
-    res.status(200).json(filteredBusiness);
-  }
 
-  next();
+  next()
 };
 
 export function filterToDelete(req, res, next) {
